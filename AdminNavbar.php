cfc7d5af57fdac7fa5e755047086e71168f03f51
RoeurBott <roland.colas@hcdc.edu.ph>
2024-10-24 00:54:41 +0800

    COMMIT

---
+++ AdminNavbar.php
@@ -0,0 +1,8 @@
+<!-- Your Navbar code -->
+<nav>
+    <ul>
+        <li><a href="admin.php">Home</a></li>
+        <li><a href="sanctions.php">Sanction</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</nav>
---
+++ navbar.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="#" onclick="showAdminModal()">Admin</a></li>
+        <li><a href="#" id="openModalNav">Reservation</a></li>
+        <li><a href="#" onclick="showBorrowModal()">Borrow Item</a></li>
+        <li><a href="StudentSanctions.php">Sanction</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</nav>
